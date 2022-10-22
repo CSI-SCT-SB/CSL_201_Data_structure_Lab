c1a06bc98c7ed21fbaa347124e57afdc5ed4d3dc
Annamjohn <108422759+Annamjohn@users.noreply.github.com>
2022-10-22 11:08:27 +0530

    Update _11.a_Bubblesort.c

--- _11.a_Bubblesort.c
+++ _11.a_Bubblesort.c
@@ -1,11 +1,5 @@
 #include<stdio.h>
 
-/*  Bubble Sort  
- - Give the array as well as n , which is the length of the array.
- - call the function, by specifying the array and the length of the array.
- - This code sorts in ascending order 
-*/
-
 int bubblesort(int arr[],int n)
  {
     for(int i=0;i<n;++i)
